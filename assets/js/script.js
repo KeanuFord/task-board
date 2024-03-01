8a2f9cbc976406475256cd642eada67c3af104e2
KeanuFord <KeanuF2017@gmail.com>
2024-03-01 15:07:58 -0800

    Added handleTask
    Modal works with successful console log., No cards yet.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,14 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+const taskModalEl = $('#task-modal');
+const taskNameInputEl = $('#task-name-input');
+const taskDateInputEl = $('#task-date-input');
+const taskDescInputEl = $('#task-desc-input');
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+  return crypto.randomUUID();
 }
 
 // Todo: create a function to create a task card
@@ -19,7 +24,20 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+  let taskName = taskNameInputEl.val();
+  let taskDate = taskDateInputEl.val();
+  let taskDesc = taskDescInputEl.val();
+  console.log("Task Name: " + taskName);
+  console.log("Task Date: " + taskDate);
+  console.log("Task Description: " + taskDesc);
 
+  const newTask = {
+    id: generateTaskId(),
+    name: taskName,
+    date: taskDate,
+    description: taskDesc,
+    status: 'to-do',
+  };
 }
 
 // Todo: create a function to handle deleting a task
@@ -33,9 +51,14 @@ function handleDrop(event, ui) {
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+
+
 $(document).ready(function () {
-    $('#taskDueDate').datepicker({
-        changeMonth: true,
-        changeYear: true,
-      });
+  
+  taskModalEl.on('submit', handleAddTask);
+
+  $('#task-date-input').datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
 });
